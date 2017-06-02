bc8f8ddc30e561ee39f7b5b2a4d7203dec009eb4
Joanna Koterba <joanna.koterba92@gmail.com>
2017-06-02 20:44:29 +0200

    Presented larger version of the chosen image

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,7 @@ export class DataService {
 
   constructor(private http:Http){}
 
-  getList(data, numPage) {
+  getList(data: any, numPage: number) {
     const params = {
       method: "flickr.photos.search",
       api_key: "7fc0d6111f95eabf19ac726a9bccb93b",
@@ -18,6 +18,19 @@ export class DataService {
     };
 
     let requestOptions = new RequestOptions({params: params});
+    return this.http.get(this.url, requestOptions);
+  }
+
+  getPhoto(id: number) {
+    const paramsPhoto = {
+      method: "flickr.photos.getInfo",
+      api_key: "7fc0d6111f95eabf19ac726a9bccb93b",
+      photo_id: id,
+      nojsoncallback: 1,
+      format: "json"
+    };
+
+    let requestOptions = new RequestOptions({params: paramsPhoto});
     return this.http.get(this.url, requestOptions);
   }
 }
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {DataService} from "../data.service";
 
 @Component({
   selector: 'app-details',
@@ -7,12 +8,16 @@ import {ActivatedRoute, Params} from "@angular/router";
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  dataPhoto: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private dataService:DataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      console.log(params);
+      this.dataService.getPhoto(params.id).subscribe(response => {
+        const data = response.json();
+        this.dataPhoto = data.photo;
+      });
     });
   }
 
